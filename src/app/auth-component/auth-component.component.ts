ac7c03d996f97124913226d490b28ef5ae8a8d63
VarakanthamSaiHruthikReddy975 <105242080+VarakanthamSaiHruthikReddy975@users.noreply.github.com>
2024-08-26 22:39:12 -0500

    First Commit - Initilized the application with various components and sub components based on primary components like Budget, Category, Transaction, User, Admin, auth, etc

---
+++ src/app/auth-component/auth-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-component.component.html',
+  styleUrl: './auth-component.component.css'
+})
+export class AuthComponentComponent {
+
+}
